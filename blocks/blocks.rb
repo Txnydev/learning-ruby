fe2145aea3f9937ee7a60954738bd297eecc41ae
Antônio Gabriel <antoniogabrielleal@gmail.com>
2019-08-11 09:12:32 -0300

    Added blocks interpretation

---
+++ blocks/blocks.rb
@@ -0,0 +1,55 @@
+# Irá executar 3 vezes este bloco.
+3.times { puts "Executando 3 vezes" }
+
+math = 0
+numbers = [5, 10, 5]
+numbers.each {|number| math += number}
+puts math
+
+foo = {2 => 3, 4 => 5}
+foo.each do |key, value|
+    puts "key #{key}"
+    puts "value #{value}"
+    puts "key * value #{key * value}"
+    puts "----"
+end
+
+def foo
+    yield
+    yield
+end
+
+foo { puts "Kkk vei..." }
+
+def foo
+    if block_given?
+        yield
+    else
+        puts "Sem parâmetro"
+    end
+end
+
+foo
+
+def foo(name, &block)
+    @name = name
+    block.call
+end
+
+foo("Antonio") { puts "Olá #{@name}" }
+
+def foo(numbers, &block)
+    if block_given?
+        numbers.each do |key, value|
+            block.call(key, value)
+        end
+    end
+end
+
+numbers = {2 => 2, 3 => 3, 4 => 4}
+
+foo(numbers) do |key, value|
+    puts "#{key} * #{value} = #{key * value}"
+    puts "#{key} + #{value} = #{key + value}"
+    puts "-----"
+end
